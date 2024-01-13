9efe12c818e0d11a07eba75a0fa71e23ab6b4ba2
Ammar Merakli <merakli.vehbi@gmail.com>
2024-01-13 09:07:18 +0100

    Update lazyvim configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.number = true
 vim.opt.title = true
 
 vim.opt.hlsearch = true
@@ -10,26 +9,15 @@ vim.opt.incsearch = true
 
 vim.opt.showcmd = true
 
-vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 5
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.smartcase = true
 
-vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
 vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.laststatus = 3
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
